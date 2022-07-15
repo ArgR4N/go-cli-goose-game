eb0690f616ba8e49c3e63b8e9752e4ae0b5c081a
ramiro <ramiroreinaldo12@gmail.com>
2022-07-15 12:02:09 -0300

    changed functions to 'method-like' functions for Game struct

--- game.go
+++ game.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"math/rand"
-	"time"
 
 	tm "github.com/buger/goterm"
 	"github.com/gookit/color"
@@ -31,7 +30,7 @@ func clear_terminal() {
 }
 
 //Init functions =>
-func get_player_quant(g *Game) {
+func (g *Game) get_player_quant() {
 	fmt.Print("Number of player (2 - 4): ")
 	n := 0
 	fmt.Scan(&n)
@@ -55,13 +54,13 @@ func get_random_case() int {
 	}
 }
 
-func init_table(g *Game) {
+func (g *Game) init_table() {
 	for index := range g.table {
 		g.table[index] = get_random_case()
 	}
 }
 
-func init_players(g *Game) {
+func (g *Game) init_players() {
 	name := ""
 	for i := 0; i < g.player_quant; i++ {
 		fmt.Printf("\t Player %X letter: ", i+1)
@@ -74,26 +73,31 @@ func init_players(g *Game) {
 	}
 }
 
-func init_game(g *Game) {
+func (g *Game) init() {
 	clear_terminal()
 
 	//Get configuration (player_quant, dificulty) =>
 	color.BgHiYellow.Println("Game configuration: ")
-	get_player_quant(g)
-	//TO_DO => get_dificulty(g)
+	g.get_player_quant()
+	//TO_DO => get_dificulty( )
 
 	//init table and players =>
-	init_table(g)
-	init_players(g)
+	g.init_table()
+	g.init_players()
 }
 
 //Init functions <=
 
 //Game loop functions =>
 func print_case(index int, num int) {
+
 	if index+1 >= 10 {
 		if num != 0 {
-			color.BgRed.Printf(" %d ", index+1)
+			if num < 0 {
+				color.BgRed.Printf(" %d ", index+1)
+			} else {
+				color.BgGreen.Printf(" %d ", index+1)
+			}
 		} else {
 			color.BgGray.Printf(" %d ", index+1)
 		}
@@ -106,7 +110,7 @@ func print_case(index int, num int) {
 	}
 }
 
-func print_table(g *Game) {
+func (g *Game) print_table() {
 	not_player := false
 	for index, elm := range g.table {
 		for i := 0; i < g.player_quant; i++ {
@@ -128,7 +132,7 @@ func print_table(g *Game) {
 	fmt.Println()
 }
 
-func turns(op string, g *Game) {
+func (g *Game) turns(op string) {
 	for i := 0; i < g.player_quant; i++ {
 		n := -1
 		if op == "sum" {
@@ -140,51 +144,55 @@ func turns(op string, g *Game) {
 
 func throw_dice() int {
 	n := rand.Intn(6) + 1
-	//color.BgGreen.Print("\nPress enter to throw the dice...")
-	//fmt.Scanln()
-	time.Sleep(3 * time.Millisecond)
+	color.BgGreen.Print("\nPress enter to throw the dice...")
+	fmt.Scanln()
+	//time.Sleep(3 * time.Millisecond)
 	clear_terminal()
 	return n
 }
 
-func move_player(dice int, p *Player) {
-	p.position += dice
+func (g *Game) move_player(dice int, index int) {
+	g.players[index].position += dice
 }
 
-func game_loop(g *Game) {
+func (g *Game) apply_special_cases(index int) {
+	if g.table[g.players[index].position] != 0 {
+		return
+	}
+}
+
+func (g *Game) start_loop() {
 	clear_terminal()
 	for g.winner == "" {
 		//Turns =>
-		turns("sum", g)
+		g.turns("sum")
 		for i := 0; i < g.player_quant; i++ {
 			if g.players[i].turn < 1 {
 				continue
 			}
-			//Each active turn =>
-			color.BgHiYellow.Printf(" Turn of %s  \n\n", g.players[i].letter)
-			print_table(g)       //Print table
-			dice := throw_dice() //Throw a dice
+			color.BgHiYellow.Printf(" Turn of %s  \n\n", g.players[i].letter) //Each active turn =>
+			g.print_table()                                                   //Print table
+			dice := throw_dice()                                              //Throw a dice
 			color.BgHiYellow.Printf(" The dice was %o |", dice)
-
-			move_player(dice, &g.players[i]) //Move correct player
-			//Aply special cases (turns +/- 1 || moves +3/-2)
+			g.move_player(dice, i)   //Move correct player
+			g.apply_special_cases(i) //Aply special cases (turns +/- 1 || moves +3/-2)
 
 			if g.players[i].position > 62 { //Check passed
-				move_player(-5, &g.players[i])
+				g.move_player(-5, i)
 			} else if g.players[i].position == 62 { //Check win
 				color.Green.Println("\nThe winner is ", g.players[i].letter, "!!")
 				g.winner = g.players[i].letter
 				break
 			}
 		}
-		turns("sub", g)
+		g.turns("sub")
 	}
 }
 
 //Game loop functions <=
 
 //For reset =>
-func ask_for_reset(sp *bool) {
+func (g *Game) ask_for_reset(sp *bool) {
 	input := ""
 	for input != "Y" && input != "N" {
 		color.BgRed.Print("\n Do you want play again?[Y/N]: ")
@@ -196,9 +204,9 @@ func ask_for_reset(sp *bool) {
 func main() {
 	still_playing := true
 	for still_playing {
-		game := Game{}                //Create new game
-		init_game(&game)              //Init game values
-		game_loop(&game)              //Start game loop
-		ask_for_reset(&still_playing) //Ask for reset => end program or reset
+		game := Game{}                     //Create new game
+		game.init()                        //Init game values
+		game.start_loop()                  //Start game loop
+		game.ask_for_reset(&still_playing) //Ask for reset => end program or reset
 	}
 }
