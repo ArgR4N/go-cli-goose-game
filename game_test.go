e17784cd672bf5ccd3a9a34c1bdcffb6deb5f7b3
ramiro <ramiroreinaldo12@gmail.com>
2022-07-28 23:46:39 -0300

    created test for get_spiral_array function

---
+++ game_test.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestGetSpiralArray(t *testing.T) {
+	assertEqual := func(t *testing.T, got [63]int, want [63]int) {
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %q, wanted %q", got, want)
+		}
+	}
+
+	got := get_sprial_array([63]int{1})
+	want := [63]int{1}
+	assertEqual(t, got, want)
+}
